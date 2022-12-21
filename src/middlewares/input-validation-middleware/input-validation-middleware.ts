250ce57c60a241332bdf8b2f09e8aa6fb6540209
Ruslan <rusishss@mail.ru>
2022-12-21 17:58:14 +0600

    подправил вывод ошибок

--- src/middlewares/input-validation-middleware/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware/input-validation-middleware.ts
@@ -3,19 +3,20 @@ import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
 
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const  errors = validationResult(req)
-    if (!errors.isEmpty()){
-        let errorsMasseges = ({errorsMessages: errors.array().map( x => {
-                    return x.msg
-        })})
-        return res.status(400).json(errorsMasseges)
 
-    // const  errors = validationResult(req).array({onlyFirstError: true}).map((item) => {
-    //     return{massage:"incorrect input",field:item.param}
-    // })
-    // if (errors.length){
-    //         let errorsMasseges = ({errorsMasseges: errors})
-    //         return res.status(400).send(errorsMasseges)
+    // const  errors = validationResult(req);
+    // if (!errors.isEmpty()){
+    //     let errorsMasseges = ({errorsMessages: errors.array().map( x => {
+    //                 return x.msg
+    //     })});
+    //     return res.status(400).json(errorsMasseges);
+
+    const  errors = validationResult(req).array({onlyFirstError: true}).map((item) => {
+        return{massage:"incorrect input",field:item.param}
+    })
+    if (errors.length){
+            let errorsMasseges = ({errorsMasseges: errors})
+            return res.status(400).send(errorsMasseges)
 
 
 
@@ -28,7 +29,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
 
     }
     else {
-        next()
+        next();
     }
 }
 
