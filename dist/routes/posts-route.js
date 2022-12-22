fbd1fbc1e66e51b2bfb3a7f24a1697b5cdf52628
Ruslan <rusishss@mail.ru>
2022-12-22 19:59:21 +0600

    убил еще 4 часа для кастомной валидации

--- dist/routes/posts-route.js
+++ dist/routes/posts-route.js
@@ -52,10 +52,10 @@ const createBlogIdValidation = (0, express_validator_1.body)('blogId')
     // }).withMessage({"message": "blogId already exist", "field": "blogId" })
     .custom((value) => __awaiter(void 0, void 0, void 0, function* () {
     const isBlogIdExist = yield posts_repository_1.postsRepository.getPostByBlogsID(value);
-    if (isBlogIdExist)
+    if (!isBlogIdExist)
         throw new Error;
     return true;
-})).withMessage({ "message": "blogId already exist", "field": "blogId" });
+})).withMessage({ "message": "blogId not exist", "field": "blogId" });
 /*
 type postType = {
     id: string,
--- src/routes/posts-route.ts
+++ src/routes/posts-route.ts
@@ -54,9 +54,9 @@ const createBlogIdValidation = body('blogId')
 
     .custom(async value => {
         const isBlogIdExist = await postsRepository.getPostByBlogsID(value)
-        if (isBlogIdExist) throw new Error
+        if (!isBlogIdExist) throw new Error
         return true
-    }).withMessage({"message": "blogId already exist", "field": "blogId" })
+    }).withMessage({"message": "blogId not exist", "field": "blogId" })
 
 
 /*
